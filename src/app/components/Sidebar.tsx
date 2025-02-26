01421dc3e34eca50b2cb6bb2d6c37802571069bc
Leong <cameron.leong@dynatrace.com>
2025-02-26 15:19:06 -0800

    Added state to sidebar items and modified text and background color

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { Accordion, SingleExpanded } from "@dynatrace/strato-components-preview/content";
 import { Spacings } from "@dynatrace/strato-design-tokens";
 import Colors from "@dynatrace/strato-design-tokens/colors";
+import Borders from '@dynatrace/strato-design-tokens/borders';
 import {
   AppHeader,
   Page,
@@ -10,16 +11,16 @@ import {
 } from '@dynatrace/strato-components-preview/layouts';
 import { AccordionComponent } from "./Accordion";
 
-//Set style for each item within the sidebar
 const SidebarItem = styled("div")<{ selected?: boolean; main?: boolean }>`
   cursor: pointer;
   padding: ${Spacings.Size8} ${Spacings.Size16};
-  border-radius: ${Spacings.Size4};
-  background-color: transparent;
+  border-radius: ${Borders.Radius.Container.Subdued};
+  background-color: ${(props) =>
+    props.selected ? Colors.Background.Container.Primary.Emphasized : 'transparent'};
   color: ${(props) =>
     props.selected
       ? props.main
-        ? Colors.Text.Neutral.Default /* No color change for main items */
+        ? Colors.Text.Neutral.DefaultActive
         : Colors.Theme.Primary[90]
       : Colors.Text.Neutral.Default};
   transition: background-color 0.3s, color 0.3s;
@@ -30,31 +31,33 @@ const SidebarItem = styled("div")<{ selected?: boolean; main?: boolean }>`
   }
 `;
 
+export const SideBar = ({ title, subtitle, items }) => {
+  const [selectedItem, setSelectedItem] = useState<string | null>(null);
 
-
-
-export const SideBar = ({title, subtitle, items}) => {
-  const [singleSection, setSingleSection] = useState<SingleExpanded>('');
   return (
-      <Page.Sidebar keepMounted>
-        <TitleBar>
-          <TitleBar.Title>{title}</TitleBar.Title>
-          <TitleBar.Subtitle>{subtitle}</TitleBar.Subtitle>
-        </TitleBar>
-        <Accordion defaultExpanded={["Anomaly detection", "Operational settings"]} multiple>
-            {items.map((section, index) => (
-              <Accordion.Section key={index} id={section.category}>
-                <Accordion.SectionLabel>{section.category}</Accordion.SectionLabel>
-                <Accordion.SectionContent>
-                  {section.items.map((item, idx) => (
-                    <SidebarItem key={idx}>
-                      {item} 
-                    </SidebarItem>
-                  ))}
-                </Accordion.SectionContent>
-              </Accordion.Section>
-            ))}
-          </Accordion>
-      </Page.Sidebar>
+    <Page.Sidebar keepMounted>
+      <TitleBar>
+        <TitleBar.Title>{title}</TitleBar.Title>
+        <TitleBar.Subtitle>{subtitle}</TitleBar.Subtitle>
+      </TitleBar>
+      <Accordion defaultExpanded={["Anomaly detection", "Operational settings"]} multiple showDividers={false}>
+        {items.map((section, index) => (
+          <Accordion.Section key={index} id={section.category}>
+            <Accordion.SectionLabel>{section.category}</Accordion.SectionLabel>
+            <Accordion.SectionContent>
+              {section.items.map((item, idx) => (
+                <SidebarItem
+                  key={idx}
+                  selected={selectedItem === item}
+                  onClick={() => setSelectedItem(item)}
+                >
+                  {item}
+                </SidebarItem>
+              ))}
+            </Accordion.SectionContent>
+          </Accordion.Section>
+        ))}
+      </Accordion>
+    </Page.Sidebar>
   );
 };
